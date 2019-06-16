0914b7da147a10100729832705611f6fd45d2353
wndeepak <deepaknishad31@gmail.com>
2019-06-16 09:32:34 +0530

    index file_changes

--- index.php
+++ index.php
@@ -1,3 +1,68 @@
-<?php
-echo phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WHERESERT</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <link rel="stylesheet" href="lib/bootstrap.min.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/website.css">
+
+    <script src="lib/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">WHERESERT</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#about">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#projects">Become A Vendor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#signup">User Sign Up/ Sign In</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<header class="masthead">
+
+    <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+            <h2 class="text">FIND A BEST DESIGNER IN YOUR CITY</h2>
+        </div>
+        <div id="form1">
+            <select class="form-control">
+                <option>Search in Dubai</option>
+            </select>
+            <form class="form-inline">
+                <div class="form-group mb-2">
+                    <label for="staticEmail2" class="sr-only">Email</label>
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail2">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword2" class="sr-only">Password</label>
+                    <input type="password" class="form-control" id="inputPassword2" placeholder="Search for a Talent/ Services/ Professional">
+                </div>
+                <button type="submit" class="btn ">FIND NOW</button>
+            </form>
+        </div>
+    </div>
+</header>
+
+
+</body>
+</html>
