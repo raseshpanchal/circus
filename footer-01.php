31697f1ed857710509cff8c70fad177b46c41c4e
Rajesh Panchal <rajeshp22@gmail.com>
2019-06-19 22:52:39 +0530

    File restructured

---
+++ footer-01.php
@@ -0,0 +1,19 @@
+<div class="container-fluid footer_01">
+    <div class="container footer_01_links">
+        About WhereSert
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Services Near Me
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Blog
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Weddings
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Homes
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Careers
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Join as a Professional
+        &nbsp;&nbsp;&nbsp;&#124;&nbsp;&nbsp;&nbsp;
+        Contact Us
+    </div>
+</div>
---
+++ footer-02.php
@@ -0,0 +1,18 @@
+<div class="container-fluid footer_02">
+    <div class="container footer_02_links">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="row">
+                    <div class="col-lg-12">COMPANY</div>
+                    <div class="col-lg-12">Review</div>
+                    <div class="col-lg-12">Coupon</div>
+                </div>
+            </div>
+            <div class="col-lg-2">SERVING IN</div>
+            <div class="col-lg-2">03</div>
+            <div class="col-lg-2">04</div>
+            <div class="col-lg-2">05</div>
+            <div class="col-lg-2">06</div>
+        </div>
+    </div>
+</div>
