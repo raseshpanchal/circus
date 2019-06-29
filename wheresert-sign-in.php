5156302747c5e5d1020537f86f7645b758398b90
Rajesh Panchal <rajeshp22@gmail.com>
2019-06-29 16:58:41 +0530

    User Login Started

--- wheresert-sign-in.php
+++ wheresert-sign-in.php
@@ -41,9 +41,20 @@
             <div class="col"></div>
 
             <div class="col-lg-6">
-                <form action="post">
+                <form name="myFormLogin" id="myFormLogin" method="POST">
 
                     <h3 style="border-bottom:dotted 1px #333; padding-bottom:15px; margin-bottom:15px">Sign-in!</h3>
+
+                    <div class="form-check-inline" style="margin-bottom:10px;">
+                        <label class="form-check-label">
+                            Login as &nbsp;&nbsp;
+                            <input type="radio" class="form-check-input" id="txt_userType" name="txt_userType" value="Freelancer" checked> Freelancer / Small Business
+                        </label>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <label class="form-check-label">
+                            <input type="radio" class="form-check-input" id="txt_userType" name="txt_userType" value="Recruiter"> Recruiter / Hirer
+                        </label>
+                    </div>
 
                     <div class="form-group">
                         <input type="text" class="form-control" id="txt_user" name="txt_user" placeholder="Enter Registered Email ID">
@@ -51,7 +62,9 @@
                     <div class="form-group">
                         <input type="password" class="form-control" id="txt_pass" name="txt_pass" placeholder="Enter Password">
                     </div>
-                    <button class="btn btnWhereSert" style="float:right">Login</button>
+                    <button class="btn btnWhereSert btnLogin" style="float:right">Login</button>
+                    <br/>
+                    <span id="loginStatus"></span>
 
                     <div class="form-group">
                         <label class="form-label">
@@ -65,8 +78,6 @@
             </div>
 
             <div class="col"></div>
-
-
 
         </div>
     </div>
@@ -107,15 +118,136 @@
     <?php include_once('footer-02.php') ?>
 
 
-
-
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
     <script>
         $(document).ready(function(){
+            $('.btnLogin').click(function(){
+                var userType = $("input[name='txt_userType']:checked").val();
+                $('.btnLogin').attr("disabled", true);
+                $('#loginStatus').html('Please wait...').fadeIn(300);
+                var myEmail=$('#txt_user').val();
+                var myPass=$('#txt_pass').val();
 
+                if(userType=='Freelancer')
+                {
+                    if(validateEmail(myEmail)!='')
+                    {
+                        if(myPass=='')
+                        {
+                            $('#loginStatus').html('<span style="color:red">Enter Password</span>');
+                            $('#txt_pass').val('');
+                            $('.btnLogin').attr("disabled", false);
+                        }
+                        else
+                        {
+                            $.post("app/freelancerLoginEntry",
+                            $("#myFormLogin").serialize(),
+                            function(data){
+                                if(data=='emailError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Wrong Email ID</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='validationError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Please Validate Your Email ID Before Login</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='passwordError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Wrong Password</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='validUser')
+                                {
+                                    $('#loginStatus').html('<span style="color:green">Account Athenticated. Please wait...</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    window.location.href='./';
+                                }
+                            });
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        $('#regiStatus').text('Please Enter Valid User Name');
+                        $('#txt_user').val('');
+                        $('.btnLogin').attr("disabled", false);
+                    }
+                }
+                else if(userType=='Recruiter')
+                {
+                    if(validateEmail(myEmail)!='')
+                    {
+                        if(myPass=='')
+                        {
+                            $('#loginStatus').html('<span style="color:red">Enter Password</span>');
+                            $('#txt_pass').val('');
+                            $('.btnLogin').attr("disabled", false);
+                        }
+                        else
+                        {
+                            $.post("app/recruiterLoginEntry",
+                            $("#myFormLogin").serialize(),
+                            function(data){
+                                if(data=='emailError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Wrong Email ID</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='validationError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Please Validate Your Email ID Before Login</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='passwordError')
+                                {
+                                    $('#loginStatus').html('<span style="color:red">Wrong Password</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    $('.btnLogin').attr("disabled", false);
+                                }
+                                else if(data=='validUser')
+                                {
+                                    $('#loginStatus').html('<span style="color:green">Account Athenticated. Please wait...</span>').fadeIn(300);
+                                    $('#login_user').val('');
+                                    $('#login_pass').val('');
+                                    window.location.href='./';
+                                }
+                            });
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        $('#regiStatus').text('Please Enter Valid User Name');
+                        $('#txt_user').val('');
+                        $('.btnLogin').attr("disabled", false);
+                    }
+                }
+                return false;
+            });
         });
+
+        //Function Email Validation
+        function validateEmail(myEmailID)
+        {
+            var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+            return re.test(myEmailID);
+        }
     </script>
 
 </body>
