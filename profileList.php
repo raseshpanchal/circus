bbd52bbd2d3ec5b05edcf400a793da50b6be3beb
Rajesh Panchal <rajeshp22@gmail.com>
2019-07-07 21:58:35 +0530

    Profile List

---
+++ profileList.php
@@ -0,0 +1,87 @@
+<?php
+    error_reporting(0);
+    //session_start();
+    include_once("config/connection.php");
+    include_once('userInfo.php');
+    include_once('pageInfo.php');
+
+    $newSubCatID=myDecode($_GET['ID']);
+    $query_subCat=mysqli_query($link, "SELECT * FROM subcategories WHERE ID='$newSubCatID'");
+    $view_subCat=mysqli_fetch_array($query_subCat);
+    $newSubCategory=$view_subCat['SubCategory'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?=$newPageTitle?></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="<?=$newPageDesc?>">
+    <meta name="keywords" content="<?=$newPageKeywords?>">
+    <?php include_once('scripts/headTags.php') ?>
+</head>
+<body>
+
+    <!--Top Menu Starts-->
+    <?php
+    include_once('topMenu.php');
+    ?>
+    <!--Top Menu Ends-->
+
+
+    <div class="container-fluid innerSlide">
+        <h1 style="margin-left:-50px"><?=$newSubCategory?></h1>
+    </div>
+
+    <div class="container">
+        <div class="row" style="padding-top:50px; padding-bottom:50px;">
+            <?php
+            $query_mainCat = mysqli_query($link, "SELECT * FROM subcategories WHERE Publish='Yes' ORDER BY SubCategory ASC");
+            while($view_mainCat=mysqli_fetch_array($query_mainCat))
+            {
+                $newCategory=$view_mainCat['Category'];
+                ?>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mainCategory"><?=$newCategory?></div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+
+    <!--Survey Info Starts-->
+    <div class="container-fluid" style="border-top:solid 2px #CCC;">
+        <?php
+        include_once('survey.php');
+        ?>
+    </div>
+    <!--Survey Info Ends-->
+
+    <!--Survey Numbers Starts-->
+    <div class="container-fluid" style="border-top:solid 2px #CCC; padding-top:20px; padding-bottom:20px;">
+        <?php
+        include_once('surveyNumbers.php');
+        ?>
+    </div>
+    <!--Survey Numbers Ends-->
+
+    <?php include_once('footer-01.php') ?>
+    <?php include_once('footer-02.php') ?>
+
+    <?php include_once('scripts/bottomScripts.php') ?>
+
+    <script>
+        $(document).ready(function(){
+
+        });
+    </script>
+
+    <script type="text/javascript">
+        function googleTranslateElementInit()
+        {
+            new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,ar,hi'}, 'google_translate_element');
+        }
+    </script>
+
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+</body>
+</html>
