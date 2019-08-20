71795a0c0317d87f4777d11461c68e036bcb7c89
holyashish <ashish.yadav384@gmail.com>
2019-08-20 13:54:38 +0530

    sql tables

--- DB/categories.sql
+++ DB/categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2019 at 07:48 AM
+-- Generation Time: Aug 20, 2019 at 10:24 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
