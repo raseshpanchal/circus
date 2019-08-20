43836029c9cddd6b061c3be5110e0c62eb155fdb
wndeepak <deepaknishad31@gmail.com>
2019-08-20 11:32:35 +0530

    footer-02 changes

--- footer-02.php
+++ footer-02.php
@@ -1,18 +1,71 @@
 <div class="container-fluid footer_02">
-    <div class="container footer_02_links">
-        <div class="row">
-            <div class="col-lg-2">
-                <div class="row">
-                    <div class="col-lg-12">COMPANY</div>
-                    <div class="col-lg-12">Review</div>
-                    <div class="col-lg-12">Coupon</div>
-                </div>
-            </div>
-            <div class="col-lg-2">SERVING IN</div>
-            <div class="col-lg-2">03</div>
-            <div class="col-lg-2">04</div>
-            <div class="col-lg-2">05</div>
-            <div class="col-lg-2">06</div>
+<!--    <div class="container footer_02_links">-->
+<!--       <div class="row text-center" style="color:#85abba">-->
+       <div class="row text-center" style="color:#ffffff">
+        <div class="col-md-4" >
+            <hr class="light">
+            <h5>COMPANY</h5>
+            <hr class="light">
+            <P>555-555-555</P>
+            <p>d123@gmail.com</p>
+            <p>100 Street Name</p>
+            <p>City, State, 0000000</p>
+        </div>
+        <div class="col-md-4">
+            <hr class="light">
+            <h5>About Us</h5>
+            <hr class="light">
+            <p>Blog</p>
+            <p>FAQ</p>
+            <p>Terms & Conditions</p>
+            <p>Policy</p>
+        </div>
+        <div class="col-md-4">
+            <hr class="light">
+            <h5>SERVING IN</h5>
+            <hr class="light">
+            <p>Dubai</p>
+            <p>Abu Dhabi</p>
+            <p>Sharjah</p>
+            <p>Bahrain</p>
+        </div>
+        <div class="col-12">
+            <h5>&copy; wheresert.com</h5>
         </div>
     </div>
 </div>
+<!--- Footer -->
+<!--
+<div class="container-fluid padding">
+    <div class="row text-center" style="background-color: #212121;">
+        <div class="col-md-4">
+            <hr class="light">
+            <h5>COMPANY</h5>
+            <hr class="light">
+            <p>Review</p>
+            <p>Coupon</p>
+            <p>Sunday: closed</p>
+        </div>
+        <div class="col-md-4">
+            <hr class="light">
+            <h5>Our Hours</h5>
+            <hr class="light">
+            <p>Monday: 9am-5pm</p>
+            <p>Saturaday: 10am-4pm</p>
+            <p>Sunday: closed</p>
+        </div>
+        <div class="col-md-4">
+            <hr class="light">
+            <h5>Service Area</h5>
+            <hr class="light">
+            <p>City, State, 0000000</p>
+            <p>City, State, 0000000</p>
+            <p>City, State, 0000000</p>
+            <p>City, State, 0000000</p>
+        </div>
+        <div class="col-12">
+            <h5>&copy; d123.com</h5>
+        </div>
+    </div>
+</div>
+-->
