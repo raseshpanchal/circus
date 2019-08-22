952775208dfd9abf67ac162cedf26523745107f4
Rajesh Panchal <rajeshp22@gmail.com>
2019-08-22 10:19:59 +0530

    Added more relevant columns

--- footer-02.php
+++ footer-02.php
@@ -1,36 +1,78 @@
 <div class="container-fluid footer_02">
-<!--    <div class="container footer_02_links">-->
-<!--       <div class="row text-center" style="color:#85abba">-->
-       <div class="row text-center" style="color:#ffffff">
-        <div class="col-md-4" >
-            <hr class="light">
-            <h5>COMPANY</h5>
-            <hr class="light">
-            <P>555-555-555</P>
-            <p>hello@wheresert.com</p>
-            <p>Shaikh Zahid Road</p>
-            <p>Dubai, UAE</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">
+                <h6>POPULAR CATEGORIES</h6>
+                <ul>
+                    <li>Academic</li>
+                    <li>Arts &amp; Crafts</li>
+                    <li>Beauty &amp; Skincare</li>
+                    <li>Hobby Classes</li>
+                    <li>Photography</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2">
+                <h6>About</h6>
+                <ul>
+                    <li>WhereSert.com</li>
+                    <li>Press &amp; News</li>
+                    <li>Partnerships</li>
+                    <li>Investor Relations</li>
+                    <li>Intellectual Property</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2">
+                <h6>Support</h6>
+                <ul>
+                    <li>Help &amp; Support</li>
+                    <li>Help &amp; Support</li>
+                    <li>FAQs</li>
+                    <li>Finding Services</li>
+                    <li>Posting Jobs</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2">
+                <h6>Community</h6>
+                <ul>
+                    <li>Blog</li>
+                    <li>Forum</li>
+                    <li>Events</li>
+                    <li>Podcast</li>
+                    <li>Invite Friend</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2">
+                <h6>Follow us</h6>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Twitter</li>
+                    <li>YouTube</li>
+                    <li>Linked-in</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2">
+                <img src="images/appStoreLogo.png" style="width:80%" />
+            </div>
+
         </div>
-        <div class="col-md-4">
-            <hr class="light">
-            <h5>About Us</h5>
-            <hr class="light">
-            <p>Blog</p>
-            <p>FAQ</p>
-            <p>Terms & Conditions</p>
-            <p>Policy</p>
-        </div>
-        <div class="col-md-4">
-            <hr class="light">
-            <h5>SERVING IN</h5>
-            <hr class="light">
-            <p>Dubai</p>
-            <p>Abu Dhabi</p>
-            <p>Sharjah</p>
-            <p>Bahrain</p>
-        </div>
-        <div class="col-12">
-            <h5>&copy; wheresert.com</h5>
+    </div>
+</div>
+
+<div class="container-fluid footer_02" style="padding-bottom:30px; font-size:10pt; color:#9C9897">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                Copyrights &#64; 2019 &nbsp;&nbsp;All Rights Reserved with WhereSert.com
+            </div>
+            <div class="col-lg-6" style="text-align:right">
+                Privacy Policy &nbsp;&nbsp;&#124;&nbsp;&nbsp; Terms &nbsp; Conditions
+            </div>
         </div>
     </div>
 </div>
