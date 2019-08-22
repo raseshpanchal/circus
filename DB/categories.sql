354778c0c264a13960a8c04bea727ee4aa5c6912
Rajesh Panchal <rajeshp22@gmail.com>
2019-08-22 12:57:50 +0530

    Provision for icons

--- DB/categories.sql
+++ DB/categories.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2019 at 10:24 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: Aug 22, 2019 at 09:21 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,6 +33,7 @@ CREATE TABLE `categories` (
   `MyID` int(11) NOT NULL,
   `MainCatID` int(11) NOT NULL,
   `Category` varchar(50) NOT NULL,
+  `Icons` varchar(50) NOT NULL,
   `Publish` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -40,34 +41,34 @@ CREATE TABLE `categories` (
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`ID`, `MyID`, `MainCatID`, `Category`, `Publish`) VALUES
-(1, 0, 2, 'Accounting', 'Yes'),
-(2, 0, 2, 'Legal', 'Yes'),
-(3, 0, 2, 'Programming ', 'Yes'),
-(4, 0, 2, 'Architecture', 'Yes'),
-(5, 0, 2, 'Engineering', 'Yes'),
-(6, 0, 2, 'Digital Designers', 'Yes'),
-(7, 0, 2, 'Beauty & Skincare', 'Yes'),
-(8, 0, 2, 'Child Care', 'Yes'),
-(9, 0, 2, 'Pet Care', 'Yes'),
-(10, 0, 2, 'Health & Well Being', 'Yes'),
-(11, 0, 2, 'Cleaning', 'Yes'),
-(12, 0, 2, 'Maintenance', 'Yes'),
-(13, 0, 2, 'Moving', 'Yes'),
-(14, 0, 2, 'Fashion Designers', 'Yes'),
-(15, 0, 2, 'Arts & Crafts', 'Yes'),
-(16, 0, 2, 'Writers & Translators', 'Yes'),
-(17, 0, 2, 'Photography', 'Yes'),
-(18, 0, 2, 'Video', 'Yes'),
-(19, 0, 2, 'Academic Classes', 'Yes'),
-(20, 0, 2, 'Hobby Classes', 'Yes'),
-(21, 0, 2, 'Language Classes', 'Yes'),
-(22, 0, 2, 'Sports Coaching', 'Yes'),
-(23, 0, 2, 'Consultants', 'Yes'),
-(24, 0, 2, 'Marketing', 'Yes'),
-(25, 0, 2, 'Entertainment', 'Yes'),
-(26, 0, 2, 'Events', 'Yes'),
-(27, 0, 2, 'Music & Audio', 'Yes');
+INSERT INTO `categories` (`ID`, `MyID`, `MainCatID`, `Category`, `Icons`, `Publish`) VALUES
+(1, 0, 2, 'Academic Classes', 'Academic.svg', 'Yes'),
+(2, 0, 2, 'Accounting', 'Accounting.svg', 'Yes'),
+(3, 0, 2, 'Architecture', 'Architecture.svg', 'Yes'),
+(4, 0, 2, 'Arts & Crafts', 'Arts.svg', 'Yes'),
+(5, 0, 2, 'Beauty & Skincare', 'BeautySkincare.svg', 'Yes'),
+(6, 0, 2, 'Child Care', 'Childcare.svg', 'Yes'),
+(7, 0, 2, 'Cleaning', 'Cleaning.svg', 'Yes'),
+(8, 0, 2, 'Consultants', 'Consulting.svg', 'Yes'),
+(9, 0, 2, 'Digital Designers', 'DigitalDesigner.svg', 'Yes'),
+(10, 0, 2, 'Engineering', 'Engineering.svg', 'Yes'),
+(11, 0, 2, 'Entertainment', 'Entertainment.svg', 'Yes'),
+(12, 0, 2, 'Events', 'Events.svg', 'Yes'),
+(13, 0, 2, 'Fashion Designers', 'FashionDesigner.svg', 'Yes'),
+(14, 0, 2, 'Health & Well Being', 'HealthWellBeing.svg', 'Yes'),
+(15, 0, 2, 'Hobby Classes', 'HobbyClasses.svg', 'Yes'),
+(16, 0, 2, 'Language Classes', 'LanguageClasses.svg', 'Yes'),
+(17, 0, 2, 'Legal', 'Legal.svg', 'Yes'),
+(18, 0, 2, 'Maintenance', 'Maintenance.svg', 'Yes'),
+(19, 0, 2, 'Marketing', 'Marketing.svg', 'Yes'),
+(20, 0, 2, 'Moving', 'Moving.svg', 'Yes'),
+(21, 0, 2, 'Music & Audio', 'Music.svg', 'Yes'),
+(22, 0, 2, 'Pet Care', 'PetCare.svg', 'Yes'),
+(23, 0, 2, 'Photography', 'Photography.svg', 'Yes'),
+(24, 0, 2, 'Programming ', 'Programming.svg', 'Yes'),
+(25, 0, 2, 'Sports Coaching', 'SportCoach.svg', 'Yes'),
+(26, 0, 2, 'Video', 'Video.svg', 'Yes'),
+(27, 0, 2, 'Writers & Translators', 'WritersTranslators.svg', 'Yes');
 
 --
 -- Indexes for dumped tables
